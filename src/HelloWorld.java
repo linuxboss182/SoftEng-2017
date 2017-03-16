45ceb4c38848b0d36a3b9a89074d93681e748c83
Michael Taylor <mttaylor@wpi.edu>
2017-03-16 17:19:21 -0400

    Added printline with my name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -10,5 +10,6 @@ public class HelloWorld {
         System.out.println("Jacob Komissar");
         System.out.println("Kenneth Levasseur");
         System.out.println("Robert Harrison");
+        System.out.println("Michael Taylor");
     }
 }
