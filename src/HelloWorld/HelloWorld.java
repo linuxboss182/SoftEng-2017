c9f07ac054e03f5b3d3a303de7a5ecc09a666cf9
Jesse Gaulin <jtgaulin@autoreg-105599.dyn.wpi.edu>
2017-03-17 11:19:25 -0400

    This is a demo for deleting a !.

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -2,7 +2,7 @@ package HelloWorld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        System.out.println("Hello World!!");
         System.out.println("Theodore Bieber");
         System.out.println("Steve McAteer");
         System.out.println("Jesse Gaulin");
