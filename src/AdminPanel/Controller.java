8002c0865673663395f360d25472e8c0d90ebb33
Jesse Gaulin <jtgaulin@autoreg-105599.dyn.wpi.edu>
2017-03-29 14:50:19 -0400

    Working on admin Panel Controller

--- src/AdminPanel/Controller.java
+++ src/AdminPanel/Controller.java
@@ -20,13 +20,90 @@ public class Controller {
     private ImageView imageViewMap;
 
     @FXML
-    private void addRoomBtnClicked() {
+    private void mapClicked() {
 
     }
 
-    @FXML
-    private void mapClicked() {
 
+
+
+    /////////////////////
+    // Stuff Ted Added //
+    /////////////////////
+
+    // Also, I changed the parameters for mapClicked
+    private ArrayList<Node> nodes = new ArrayList<>();
+    private int clickedX;
+    private int clickedY;
+
+    // This theoretically should be called
+    // It sets up the listeners
+    public Controller() {
+        // Clicking on the floor image
+        imageViewMap.setPickOnBounds(true); // allow detection of clicks on transparent parts of image
+        imageViewMap.setOnMouseClicked(e -> {
+            mapClicked((int) e.getX(), (int) e.getY());
+        });
+
+        // Add Room Button Pressed
+        addRoomBtn.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                System.out.println("Clicked the button");
+                addRoomBtnClicked();
+            }
+        });
+    }
+
+    @FXML
+    private void addRoomBtnClicked() {
+        Node n = new Node(readXPosField(), readYPosField(), readNameField(), readDescriptionField());
+        nodes.add(n);
+        System.out.println("Added a new node to the buffer");
+    }
+
+    @FXML
+    private void mapClicked(int x, int y) {
+        updatePositions(x, y);
+        System.out.println("Clicked (" + x + ", " + y + ")");
+    }
+
+    //TODO: ADDS THE NODES TO THE DATABASE!!! -- Requires DatabaseController
+    private void flushbuffer() {
+
+    }
+
+    private void updatePositions(int x, int y) {
+        clickedX = x;
+        clickedY = y;
+        setXPosText("" + x);
+        setYPosText("" + y);
+        System.out.println("Updated the X and Y text fields to (" + clickedX + ", " + clickedY + ")");
+    }
+
+    private void setXPosText(String text) {
+        XcoordField.setText(text);
+    }
+
+    private void setYPosText(String text) {
+        YcoordField.setText(text);
+    }
+
+    private int readXPosField() {
+        String raw = XcoordField.getText();
+        return Integer.parseInt(raw);
+    }
+
+    private int readYPosField() {
+        String raw = YcoordField.getText();
+        return Integer.parseInt(raw);
+    }
+
+    private String readNameField() {
+        return nameField.getText();
+    }
+
+    private String readDescriptionField() {
+        return descriptField.getText();
     }
 
 }
---
+++ src/Entities/Node.java
@@ -0,0 +1,50 @@
+package Entities;
+
+/**
+ * Created by jtgaulin on 3/29/17.
+ */
+public class Node {
+    public Node(int x, int y, String name, String desc) {
+        this.x = x;
+        this.y = y;
+        this.name = name;
+        this.desc = desc;
+    }
+
+    private int x;
+    private int y;
+    private String name;
+    private String desc;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
