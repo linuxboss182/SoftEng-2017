092a446355e08bd67668b4c647b23bd6d4525a6c
TheodoreJBieber <tbaslp@yahoo.com>
2017-03-30 19:31:21 -0400

    Did a little more work on directory. There is one method still unwritten: getRoomData. Additionally, if anyone wants to use a regular array instead of an arraylist, @ message me on slack and we can talk about it

--- src/main/java/entities/Directory.java
+++ src/main/java/entities/Directory.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.util.ArrayList;
+
 /**
  * The class for a Directory
  * //TODO: I made this based off of the class diagram as best I could but there's still stuff to do
@@ -10,34 +12,40 @@ public class Directory
 
 	// A barren constructor for Directory, currently doesn't do anything differently than normal
 	public Directory() {
-
+		allRooms = new ArrayList<>();
 	}
 
 	// Methods
 
-	//TODO: The parameters and return type need to be figured out
+	//TODO: make this work
 	public void getRoomData(Object n) {
 
 	}
 
-	// TODO: Write this method
+	/**
+	 * Adds a node to the list
+	 * @param newNode The node that is being added to the list
+	 */
 	public void addNode(Node newNode) {
-
+		allRooms.add(newNode);
 	}
 
-	// TODO: Write this method. Also, it is common for removal functions to return the Object. Change return type to Node?
-	public void removeNode(Node node) {
-
+	/**
+	 * Removes a node from the list
+	 * @param node The reference to the node that is being removed. This must be the same reference as the one in the list
+	 * @return True: if the node has successfully been removed False: otherwise
+	 */
+	public boolean removeNode(Node node) {
+		return allRooms.remove(node);
 	}
 
-	// TODO: Write this method
+	// TODO: Write this method properly
 	private int getHeight() {
-		return -1;
+		return 4;
 	}
 
 	// Attributes
 
-	// TODO: This could be implemented as an ArrayList<Node> to make it a lot easier to modify
-	private Node[] allRooms;
+	private ArrayList<Node> allRooms;
 
 }
