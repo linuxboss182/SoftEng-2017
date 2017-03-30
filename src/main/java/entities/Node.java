bf1f3eb8d1f52c7fc0509d5c89f405a22ec22221
Jacob I. Komissar <git.i@komissar.net>
2017-03-30 14:25:49 -0400

    Finish making code compliant

--- src/main/java/entities/Node.java
+++ src/main/java/entities/Node.java
@@ -3,47 +3,47 @@ package entities;
 
 public class Node
 {
-    private int x;
-    private int y;
-    private String name;
-    private String desc;
+	private int x;
+	private int y;
+	private String name;
+	private String desc;
 
-    public Node(int x, int y, String name, String desc) {
-        this.x = x;
-        this.y = y;
-        this.name = name;
-        this.desc = desc;
-    }
+	public Node(int x, int y, String name, String desc) {
+		this.x = x;
+		this.y = y;
+		this.name = name;
+		this.desc = desc;
+	}
 
-    public int getX() {
-        return x;
-    }
+	public int getX() {
+		return x;
+	}
 
-    public int getY() {
-        return y;
-    }
+	public int getY() {
+		return y;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getDesc() {
-        return desc;
-    }
+	public String getDesc() {
+		return desc;
+	}
 
-    public void setX(int x) {
-        this.x = x;
-    }
+	public void setX(int x) {
+		this.x = x;
+	}
 
-    public void setY(int y) {
-        this.y = y;
-    }
+	public void setY(int y) {
+		this.y = y;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
 }
