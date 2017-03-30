ada598a5fd98e845f190253f290a64d5f42d1b74
TheodoreJBieber <tbaslp@yahoo.com>
2017-03-30 17:00:48 -0400

    Started writing the Directory entity, though it doesn't have any functionality yet. Also created the entities branch, for developing the entities

---
+++ src/main/java/entities/Directory.java
@@ -0,0 +1,43 @@
+package entities;
+
+/**
+ * The class for a Directory
+ * //TODO: I made this based off of the class diagram as best I could but there's still stuff to do
+ */
+public class Directory
+{
+	// Constructors
+
+	// A barren constructor for Directory, currently doesn't do anything differently than normal
+	public Directory() {
+
+	}
+
+	// Methods
+
+	//TODO: The parameters and return type need to be figured out
+	public void getRoomData(Object n) {
+
+	}
+
+	// TODO: Write this method
+	public void addNode(Node newNode) {
+
+	}
+
+	// TODO: Write this method. Also, it is common for removal functions to return the Object. Change return type to Node?
+	public void removeNode(Node node) {
+
+	}
+
+	// TODO: Write this method
+	private int getHeight() {
+		return -1;
+	}
+
+	// Attributes
+
+	// TODO: This could be implemented as an ArrayList<Node> to make it a lot easier to modify
+	private Node[] allRooms;
+
+}
