254a5fe4cbb696c99563cbbb70c25d5b100961fd
scioniv <knl129847>
2017-03-30 10:15:21 -0400

    Setup database controller with constructors and schema initialization. Added a Main class for controller testing.

--- src/Controllers/DatabaseController.java
+++ src/Controllers/DatabaseController.java
@@ -5,75 +5,105 @@
 package Controllers;
 import java.sql.*;
 
+/**
+ * Created by Kenneth on 3/28/17.
+ */
+
 public class DatabaseController {
-    public static void main(String[] args) {
-        System.out.println("-------Embedded Java DB Connection Testing --------");
+
+    private Connection db_connection;
+    private String connection_string;
+
+    private final String nodes = "CREATE TABLE Nodes (\n" +
+            "    nodeID integer PRIMARY KEY\n" +
+            "  , nodeX  integer\n" +
+            "  , nodeY  integer\n" +
+            ");";
+    private final String edges = "CREATE TABLE Edges (\n" +
+            "    node1 integer references Nodes(nodeID) NOT NULL ON DELETE CASCADE\n" +
+            "  , node2 integer references Nodes(nodeID) NOT NULL ON DELETE CASCADE\n" +
+            ");";
+    private final String rooms = "CREATE TABLE Rooms (\n" +
+            "    roomName        varchar(200) PRIMARY KEY\n" +
+            "  , roomDescription varchar(1000)\n" +
+            "  , nodeID          integer references Nodes(nodeID)\n" +
+            ");";
+    private final String employees = "CREATE TABLE Employees (\n" +
+            "    employeeID        integer PRIMARY KEY\n" +
+            "  , employeeGivenName varchar(100)\n" +
+            "  , employeeSurname   varchar(100)\n" +
+            "  , employeeTitle     varchar(100)\n" +
+            ");";
+
+    private final String employee_rooms = "CREATE TABLE EmployeeRooms (\n" +
+            "    roomName   varchar(200) references Rooms(roomName)\n" +
+            "  , employeeID integer references Employees(employeeID)\n" +
+            "  , constraint EmployeeRooms_pk PRIMARY KEY (roomName, employeeID)\n" +
+            ");";
+
+    public DatabaseController(){
+        db_connection = null;
+        this.connection_string = "jdbc:derby:DB;create=true";
+
         try {
             Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-        } catch (ClassNotFoundException e) {
+        } catch(ClassNotFoundException e) {
             System.out.println("Java DB Driver not found. Add the classpath to your module.");
-            System.out.println("For IntelliJ do the following:");
-            System.out.println("File | Project Structure, Modules, Dependency tab");
-            System.out.println("Add by clicking on the green plus icon on the right of the window");
-            System.out.println("Select JARs or directories. Go to the folder where the Java JDK is installed");
-            System.out.println("Select the folder java/jdk1.8.xxx/db/lib where xxx is the version.");
-            System.out.println("Click OK, compile the code and run it.");
-            e.printStackTrace();
-            return;
         }
-
-        System.out.println("Java DB driver registered!");
-        Connection connection = null;
+        System.out.println("Stuff works");
 
         try {
-            connection = DriverManager.getConnection("jdbc:derby:DB;create=true");
+            // substitute your database name for myDB
+            db_connection = DriverManager.getConnection(this.connection_string);
         } catch (SQLException e) {
             System.out.println("Connection failed. Check output console.");
             e.printStackTrace();
             return;
         }
         System.out.println("Java DB connection established!");
-
-
-
+        initSchema();
     }
+
+    public DatabaseController(String connection_string){
+        db_connection = null;
+        this.connection_string = connection_string;
+
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+        } catch(ClassNotFoundException e) {
+            System.out.println("Java DB Driver not found. Add the classpath to your module.");
+        }
+        System.out.println("Stuff works");
+
+        try {
+            // substitute your database name for myDB
+            db_connection = DriverManager.getConnection(this.connection_string);
+        } catch (SQLException e) {
+            System.out.println("Connection failed. Check output console.");
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("Java DB connection established!");
+    }
+    //db get/set methods below
+
+    //initializes the database empty with the desired schema
+    //returns true if success, false if error
+    private boolean initSchema(){
+        try {
+            boolean result;
+            Statement init_schema = this.db_connection.createStatement();
+            result = init_schema.execute(nodes); //no need for result set here
+            result = init_schema.execute(edges);
+            result = init_schema.execute(rooms);
+            result = init_schema.execute(employees);
+            result = init_schema.execute(employee_rooms);
+        } catch(SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    //TODO init data
+
 }
-
-
-
-
-//package Controllers;
-//import java.sql.*;
-//
-///**
-// * Created by Kenneth on 3/28/17.
-// */
-//
-//public class DBController {
-//
-//    private Connection db_connection;
-//
-//    public DBController(String connection_string){
-//        db_connection = null;
-//
-//        try {
-//            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-//        } catch(ClassNotFoundException e) {
-//            System.out.println("Java DB Driver not found. Add the classpath to your module.");
-//        }
-//        System.out.println("Stuff works");
-//
-//        try {
-//            // substitute your database name for myDB
-//            db_connection = DriverManager.getConnection(connection_string);
-//        } catch (SQLException e) {
-//            System.out.println("Connection failed. Check output console.");
-//            e.printStackTrace();
-//            return;
-//        }
-//        System.out.println("Java DB connection established!");
-//
-//
-//    }
-//    //db get/set methods below
-//}
---
+++ src/Controllers/Main.java
@@ -0,0 +1,10 @@
+package Controllers;
+
+/**
+ * Created by Kenneth on 3/30/17.
+ */
+public class Main{
+    public static void main(String[] args){
+        DatabaseController dbc = new DatabaseController();
+    }
+}
