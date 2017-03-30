21e4525dc29537d279371f793914046afb50cf08
Jesse Gaulin <jtgaulin@autoreg-105599.dyn.wpi.edu>
2017-03-30 14:47:04 -0400

    Merged with master

--- src/main/java/main/ApplicationController.java
+++ src/main/java/main/ApplicationController.java
@@ -5,7 +5,6 @@ import javafx.application.Application;
 public class ApplicationController {
 
 	public static void main(String[] args) {
-
 		Application.launch(userpanel.Window.class, args);
 		DatabaseController dbc = new DatabaseController();
 	}
