cb042ca01cb35ee9fadc3c08ca41323515fa71ce
TheodoreJBieber <tbaslp@yahoo.com>
2017-03-31 14:48:44 -0400

    oddly it isn't working... changed some stuff

--- src/main/java/main/Pathfinder.java
+++ src/main/java/main/Pathfinder.java
@@ -27,23 +27,48 @@ public class Pathfinder
 
 		for(int i = 1; i < path.length - 1; i++) {
 			double turnAngle = path[i].angle(path[i-1], path[i+1]);
+			int leftTurns = 0, rightTurns = 0;
 
 			// Determine the direction of the turn through a bunch of if statements
 			if(turnAngle >= rightMin1 && turnAngle < rightMax1 || turnAngle >= rightMin2 && turnAngle < rightMax2) {
 				// Right Turn
-				directions += "take a right turn,\nThen ";
+				if(rightTurns == 0) {
+					directions += "take a right turn,\nThen ";
+				} else {
+					directions += "take the " + rightTurns + "rd Right,\n Then";
+					rightTurns = 0;
+				}
 			} else if(turnAngle >= rightMax2 && turnAngle < softRightMax) {
 				// Soft Right Turn
 				directions += "take a soft right turn,\nThen ";
 			} else if(turnAngle >= softRightMax && turnAngle < straightMax) {
 				// Straight (NO TURN!!!)
 				directions += "continue straight,\nThen ";
+				// Figure out if there is a left or right turn available as well, then increment the counters
+				Node[] forks = path[i].getAdjacencies();
+				for(int j = 0; j < forks.length; j++) {
+					double forkAngle = path[i].angle(path[i-1], forks[j]);
+					// Only checks for normal right and left turns (not including soft or hard variants)
+					if(forkAngle >= rightMin1 && forkAngle < rightMax1 || forkAngle >= rightMin2 && turnAngle < rightMax2) {
+						rightTurns++;
+					}
+					if(forkAngle >= softLeftMax && forkAngle < leftMax) {
+						leftTurns++;
+					}
+				}
+
+
 			} else if(turnAngle >= straightMax && turnAngle < softLeftMax) {
 				// Soft Left Turn
 				directions += "take a soft left turn\nThen ";
 			} else if(turnAngle >= softLeftMax && turnAngle < leftMax) {
 				// Left Turn
-				directions += "take a left turn\nThen ";
+				if(leftTurns == 0) {
+					directions += "take a left turn,\nThen ";
+				} else {
+					directions += "take the " + leftTurns + "rd Left,\n Then";
+					leftTurns = 0;
+				}
 			} else if(turnAngle >= leftMax && turnAngle < hardLeftMax) {
 				// Hard Left Turn
 				directions += "take a hard left turn\nThen ";
@@ -60,7 +85,7 @@ public class Pathfinder
 	}
 
 	public static void main(String[] args) {
-		Node[] path = {new Node(1,1), new Node(1, 2), new Node(2,3), new Node(2, 4), new Node(1, 3), new Node(0, 4), new Node(-1,5), new Node(-2, 6), new Node(-3, 7)};
+		Node[] path = {new Node(1,1), new Node(1, 2), new Node(2,3), new Node(2, 4), new Node(1, 3), new Node(0, 4), new Node(-1,5), new Node(-2, 6), new Node(-3, 7), new Node(-2, 8)};
 		System.out.println(getTextDirections(path));
 	}
 }
