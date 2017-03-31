b1e779745a4f11c07a56d4c17e45ec0aeeb917ee
Robert Harrison <raharrison2@wpi.edu>
2017-03-31 14:40:26 -0400

    Add Pathfinder class skeleton

---
+++ src/main/java/algorithms/Pathfinder.java
@@ -0,0 +1,19 @@
+package algorithms;
+
+import entities.Node;
+
+import java.util.List;
+
+/**
+ */
+public class Pathfinder {
+	private Node origin;
+	private Node destination;
+
+	// A* method for pathfinding through the hospital
+	public List<Node> findPath(Node start, Node finish){
+
+
+	}
+
+}
