d593855464169c2cd6ead8fa0e86c9e0b390e5f2
TheodoreJBieber <tbaslp@yahoo.com>
2017-04-02 12:51:00 -0400

    Add Function to LoginPrompt

--- src/main/java/adminpanel/LoginController.java
+++ src/main/java/adminpanel/LoginController.java
@@ -1,6 +1,28 @@
 package adminpanel;
 
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
 public class LoginController
 {
+
+	@FXML
+	public void loginClicked() throws IOException, InvocationTargetException {
+		Parent adminUI = (BorderPane) FXMLLoader.load(getClass().getResource("/AdminUI.fxml"));
+		System.out.print("Onto the Admin UI");
+		Scene adminScene = new Scene(adminUI);
+		Stage adminStage = new Stage();
+		adminStage.setScene(adminScene);
+
+		adminStage.showAndWait();
+	}
 }
