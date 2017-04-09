9a5737f7e568ecc66817f1fd01636c3af291d933
TheodoreJBieber <tbaslp@yahoo.com>
2017-04-08 23:49:32 -0400

    fix Find Path Now button
    had to link the UserPath fxml to UserPathController instead of UserController

--- src/main/java/controllers/UserMasterController.java
+++ src/main/java/controllers/UserMasterController.java
@@ -148,8 +148,11 @@ public abstract class UserMasterController extends MapDisplayController
 
 	@FXML
 	public void getDirectionsClicked() throws IOException, InvocationTargetException {
+		System.out.println("UserMasterController.getDirectionsClicked");
 		Parent userPath = (BorderPane) FXMLLoader.load(this.getClass().getResource("/UserPath.fxml"));
-		this.sideGridPane.getScene().setRoot(userPath);
+		System.out.println("UserMasterController.getDirectionsClicked");
+		this.getDirectionsBtn.getScene().setRoot(userPath);
+		System.out.println("UserMasterController.getDirectionsClicked");
 	}
 
 
