a530ded605185fe2cabf0f473e2dc3001bb35249
Stovetop <stevenmcateer7@gmail.com>
2017-04-09 22:13:11 -0400

    Redesign AdminUI

--- src/main/java/controllers/EditorController.java
+++ src/main/java/controllers/EditorController.java
@@ -12,10 +12,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -48,7 +45,7 @@ public class EditorController extends MapDisplayController implements Initializa
 	@FXML
 	private TextField nameField;
 	@FXML
-	private TextField descriptField;
+	private TextArea descriptField;
 	@FXML
 	private TextField xCoordField;
 	@FXML
