5b6c75cdc006f67bf5d26bcbb0f3551c0e8f0027
Jesse Gaulin <jtgaulin@autoreg-105599.dyn.wpi.edu>
2017-04-12 21:28:54 -0400

    Added controller for SMS

--- src/main/java/controllers/SMSController.java
+++ src/main/java/controllers/SMSController.java
@@ -3,25 +3,57 @@ package controllers;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-/**
- * Created by jtgaulin on 4/12/17.
- */
-public class SMSController
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SMSController implements Initializable
 {
 
-	public SMSController(){
-	String accountSid = "ACf281d4ea0146ff8ba3b28d0451ef534c"; // Your Account SID from www.twilio.com/user/account
-	String authToken = "6ce6450f4fdf3ce3318433197eead0cf"; // Your Auth Token from www.twilio.com/user/account
+	@FXML
+	private Button cancelBtn;
+	@FXML
+	private Button SendBtn;
+	@FXML
+	private TextField phoneNumField;
+	@FXML
+	private Label errorLabel;
 
-	Twilio.init(accountSid, authToken);
-
-	Message message = Message.creator(
-			new PhoneNumber("+15086851848"),  // To number
-			new PhoneNumber("+15086199280"),  // From number
-			"Hello world!"                    // SMS body
-	).create();
-
-	System.out.println(message.getSid());
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
 	}
+
+	@FXML
+	public void onSendBtnClicked() {
+		try {
+			String accountSid = "ACf281d4ea0146ff8ba3b28d0451ef534c"; // Your Account SID from www.twilio.com/user/account
+
+			String authToken = "6ce6450f4fdf3ce3318433197eead0cf"; // Your Auth Token from www.twilio.com/user/account
+
+			Twilio.init(accountSid, authToken);
+
+			Message message = Message.creator(
+					new PhoneNumber("+15086851848"),  // To number
+					new PhoneNumber("+15086199280"),  // From number
+					"Hello world!"                    // SMS body
+			).create();
+
+			System.out.println(message.getSid());
+			SendBtn.getScene().getWindow().hide();
+		} catch(Exception e){
+			this.errorLabel.setText("Error Sending SMS");
+		}
+	}
+
+	@FXML
+	public void onCancelBtnClick(){
+		cancelBtn.getScene().getWindow().hide();
+	}
+
+
 }
