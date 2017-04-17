cd5c4659fabe33a2c30a98aa1adc30bbe2be1b14
Robert Harrison <raharrison2@wpi.edu>
2017-04-17 16:31:30 -0400

    Edited layout of the about page.
    Added a stub for the about page Controller
    
    Added parrot gifs too

---
+++ src/main/java/controllers/user/UserAboutPage.java
@@ -0,0 +1,15 @@
+package controllers.user;
+
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UserAboutPage
+{
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		};
+}
