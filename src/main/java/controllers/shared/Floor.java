2ce921214e3bddc0af34cec90caf349a8e3dacc2
Robert Harrison <raharrison2@wpi.edu>
2017-04-18 19:29:28 -0400

    Changed floors again

--- src/main/java/controllers/shared/Floor.java
+++ src/main/java/controllers/shared/Floor.java
@@ -3,6 +3,7 @@ package controllers.shared;
 import javafx.scene.image.Image;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Floor implements FloorImage
@@ -12,10 +13,25 @@ public class Floor implements FloorImage
 	private String path;
 	private String thumbPath;
 	// Linked list containing the string names of the different floor images.
-	private LinkedList<String> floorImages = new LinkedList<>(Arrays.asList(
-							  "/1_thefirstfloor.png", "/2_thesecondfloor.png", "/3_thethirdfloor.png",
-							  "/4_thefourthfloor.png", "/5_thefifthfloor.png", "/6_thesixthfloor.png",
-							  "/7_theseventhfloor.png"));
+	private LinkedList<String> faulknerFloorImages = new LinkedList<>(Arrays.asList(
+			"/1_thefirstfloor.png", "/2_thesecondfloor.png", "/3_thethirdfloor.png",
+			"/4_thefourthfloor.png", "/5_thefifthfloor.png", "/6_thesixthfloor.png",
+			"/7_theseventhfloor.png"));
+
+	private LinkedList<String> belkinFloorImages = new LinkedList<>(Arrays.asList(
+			"/belkin1.png", "/belkin2.png", "/belkin3.png", "/belkin4.png"));
+
+	private LinkedList<String> outsideFloorImages = new LinkedList<>(Arrays.asList(
+			"./outsidearea.png"));
+
+	private HashMap<String, LinkedList<String>> floorImages = new HashMap<>();
+
+	private HashMap<String, LinkedList<String>> getFlImg(){
+		floorImages.put("Faulkner", faulknerFloorImages);
+		floorImages.put("Belkin", belkinFloorImages);
+		floorImages.put("Outside", outsideFloorImages);
+		return floorImages;
+	}
 
 	// Linked list containing the string names of the different floor images.
 	private LinkedList<String> thumbnails = new LinkedList<>(Arrays.asList(
@@ -24,15 +40,15 @@ public class Floor implements FloorImage
 			"/Thumb_Seventh_Floor.png"));
 
 	// constructor for the floor class
-	public Floor(int floorNum) {
+	public Floor(String building, int floorNum) {
 		this.floorNum = floorNum;
-		this.path = floorImages.get(floorNum - 1);
+		this.path = getFlImg().get(building).get(floorNum - 1);
 		this.thumbPath = thumbnails.get(floorNum - 1);
 	}
 
 	/** takes the String name of the path attribute and loads the image attached to that path
 	 *
- 	 * @return an image of the specified path.
+	 * @return an image of the specified path.
 	 * TODO: The image doesn't load sometimes. Having it load in the background prevents it from crashing
 	 */
 	public Image display() {
