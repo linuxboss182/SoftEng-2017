60a134e71c06716cd0dde67e04ece9debace9c6c
Jesse Gaulin <jtgaulin@autoreg-105599.dyn.wpi.edu>
2017-04-18 21:44:13 -0400

    Updated UserMasterController to handle new zoom

--- src/main/java/controllers/user/UserMasterController.java
+++ src/main/java/controllers/user/UserMasterController.java
@@ -10,6 +10,7 @@ import javafx.collections.FXCollections;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Bounds;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
@@ -17,6 +18,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
@@ -88,12 +90,15 @@ public abstract class UserMasterController
 	private Button aboutBtn;
 	@FXML
 	private ImageView logoImageView;
+	@FXML
+	private ScrollPane mapScroll = new ScrollPane();
 
 	final double SCALE_DELTA = 1.1;
 	final protected double zoomMin = 1/SCALE_DELTA;
 	final protected double zoomMax = SCALE_DELTA*5;
 
 	private double clickedX, clickedY;
+	protected static double SCALE_TOTAL = 1;
 	protected static Room startRoom;
 	protected static Room endRoom;
 	protected static boolean choosingStart = false;
@@ -143,25 +148,25 @@ public abstract class UserMasterController
 		this.enableOrDisableNavigationButtons();
 
 		// I tested this value, and we want it to be defaulted here because the map does not start zoomed out all the way
-		zoomSlider.setValue(2);
-		zoomSlider.valueProperty().addListener(new ChangeListener<Number>() {
-			@Override
-			public void changed(ObservableValue<? extends Number> observable,
-			                    Number oldValue, Number newValue) {
-				/**
-				 * This one was a fun one.
-				 * This math pretty much makes it so when the slider is at the far left, the map will be zoomed out all the way
-				 * and when it's at the far right, it will be zoomed in all the way
-				 * when it's at the left, zoomPercent is 0, so we want the full value of zoomMin to be the zoom coefficient
-				 * when it's at the right, zoomPercent is 1, and we want the full value of zoomMax to be the zoom coefficient
-				 * the equation is just that
-				 */
-				double zoomPercent = (zoomSlider.getValue()/100);
-				double zoomCoefficient = zoomMin*(1 - zoomPercent) + zoomMax*(zoomPercent);
-				contentAnchor.setScaleX(zoomCoefficient);
-				contentAnchor.setScaleY(zoomCoefficient);
-			}
-		});
+//		zoomSlider.setValue(2);
+//		zoomSlider.valueProperty().addListener(new ChangeListener<Number>() {
+//			@Override
+//			public void changed(ObservableValue<? extends Number> observable,
+//			                    Number oldValue, Number newValue) {
+//				/**
+//				 * This one was a fun one.
+//				 * This math pretty much makes it so when the slider is at the far left, the map will be zoomed out all the way
+//				 * and when it's at the far right, it will be zoomed in all the way
+//				 * when it's at the left, zoomPercent is 0, so we want the full value of zoomMin to be the zoom coefficient
+//				 * when it's at the right, zoomPercent is 1, and we want the full value of zoomMax to be the zoom coefficient
+//				 * the equation is just that
+//				 */
+//				double zoomPercent = (zoomSlider.getValue()/100);
+//				double zoomCoefficient = zoomMin*(1 - zoomPercent) + zoomMax*(zoomPercent);
+//				contentAnchor.setScaleX(zoomCoefficient);
+//				contentAnchor.setScaleY(zoomCoefficient);
+//			}
+//		});
 
 		if(floorChoiceBox != null) {
 			initFloorChoiceBox();
@@ -184,17 +189,38 @@ public abstract class UserMasterController
 						(event.getDeltaY() > 0)
 								? SCALE_DELTA
 								: 1/SCALE_DELTA;
-				double potentialScaleX = contentAnchor.getScaleX() * scaleFactor;
-				double potentialScaleY = contentAnchor.getScaleY() * scaleFactor;
-				// Pretty much just limit the scaling minimum and maximum
-				potentialScaleX = (potentialScaleX < zoomMin ? zoomMin:potentialScaleX);
-				potentialScaleY = (potentialScaleY < zoomMin ? zoomMin:potentialScaleY);
-				potentialScaleX = (potentialScaleX > zoomMax ? zoomMax:potentialScaleX);
-				potentialScaleY = (potentialScaleY > zoomMax ? zoomMax:potentialScaleY);
-				contentAnchor.setScaleX(potentialScaleX);
-				contentAnchor.setScaleY(potentialScaleY);
-				// Update the slider
-				zoomSlider.setValue(((potentialScaleX - zoomMin) / (zoomMax - zoomMin))*100);
+
+				if (scaleFactor * SCALE_TOTAL >= 1 && scaleFactor * SCALE_TOTAL <= 6) {
+					Bounds viewPort = mapScroll.getViewportBounds();
+					Bounds contentSize = contentAnchor.getBoundsInParent();
+
+					double centerPosX = (contentSize.getWidth() - viewPort.getWidth()) * mapScroll.getHvalue() + viewPort.getWidth() / 2;
+
+					double centerPosY = (contentSize.getHeight() - viewPort.getHeight()) * mapScroll.getVvalue() + viewPort.getHeight() / 2;
+
+					mapScroll.setScaleX(mapScroll.getScaleX() * scaleFactor);
+					mapScroll.setScaleY(mapScroll.getScaleY() * scaleFactor);
+					SCALE_TOTAL *= scaleFactor;
+
+					double newCenterX = centerPosX * scaleFactor;
+					double newCenterY = centerPosY * scaleFactor;
+
+					mapScroll.setHvalue((newCenterX - viewPort.getWidth() / 2) / (contentSize.getWidth() * scaleFactor - viewPort.getWidth()));
+					mapScroll.setVvalue((newCenterY - viewPort.getHeight() / 2) / (contentSize.getHeight() * scaleFactor - viewPort.getHeight()));
+				}
+
+				if (scaleFactor * SCALE_TOTAL <= 1) {
+//					SCALE_TOTAL = 1/scaleFactor;
+					zoomSlider.setValue(0);
+
+				}else if(scaleFactor * SCALE_TOTAL >= 5.5599173134922495) {
+//					SCALE_TOTAL = 6 / scaleFactor;
+					zoomSlider.setValue(100);
+
+				}else {
+					zoomSlider.setValue(((SCALE_TOTAL - 1)/4.5599173134922495) * 100);
+				}
+
 			}
 		});
 		contentAnchor.setOnMousePressed(new EventHandler<MouseEvent>() {
