aeaf1b59f3888b047d1c50f64de33ebe724f36c0
Jacob I. Komissar <git.i@komissar.net>
2017-04-25 12:45:07 -0400

    Deprecate various Icon methods

--- src/main/java/entities/icons/Icon.java
+++ src/main/java/entities/icons/Icon.java
@@ -9,6 +9,7 @@ import javafx.scene.image.ImageView;
 public class Icon
 		extends Group
 {
+	@Deprecated
 	private Node symbol;
 	private Label label;
 	private ImageView image;
@@ -21,38 +22,47 @@ public class Icon
 		this.label = label;
 	}
 
+	@Deprecated
 	public Icon(Node symbol, Label label) {
 		super(symbol, label);
 		this.symbol = symbol;
 		this.label = label;
 	}
 
+	@Deprecated
 	Icon(Node symbol) {
 		this(symbol, null);
 	}
 
+	@Override
 	public void relocate(double x, double y) {
-		this.symbol.setLayoutX(x);
-		this.symbol.setLayoutY(y);
+		this.image.relocate(x, y);
+		this.label.relocate(x, y);
 	}
 
+	@Deprecated
 	public Node getSymbol() {
 		return this.symbol;
 	}
 
+	@Deprecated
 	public Label getLabel() {
 		return this.label;
 	}
 
 
+	@Deprecated
 	void setSymbol(Node symbol) {
 		this.symbol = symbol;
 	}
 
+	@Deprecated
 	void setLabel(Label label) {
 		this.label = label;
 	}
 
+	/** @deprecated Use relocate() instead */
+	@Deprecated
 	public void moveTo(double x, double y) {
 		this.image.setLayoutX(x);
 		this.image.setLayoutY(y);
