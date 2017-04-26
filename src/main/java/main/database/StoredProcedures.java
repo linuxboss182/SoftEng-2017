b4f4059970ae55fa0d4202d6ecc5b28ebafad291
Jesse Gaulin <jtgaulin@autoreg-105599.dyn.wpi.edu>
2017-04-25 22:09:41 -0400

    Fixed sql procedures

--- src/main/java/main/database/StoredProcedures.java
+++ src/main/java/main/database/StoredProcedures.java
@@ -27,7 +27,7 @@ public class StoredProcedures
 					+" , roomDescription varchar(1000)"
 					+" , labelX double precision NOT NULL"
 					+" , labelY double precision NOT NULL"
-					+" , nodeID          integer references Nodes(nodeID) ON DELETE SET NULL)"
+					+" , nodeID          integer references Nodes(nodeID) ON DELETE SET NULL"
 					+" , roomType   varchar(100) NOT NULL)",
 			"CREATE TABLE Employees ("
 					+" employeeID        integer PRIMARY KEY"
@@ -174,14 +174,14 @@ public class StoredProcedures
 		roomDescription = sanitize(roomDescription);
 		roomType = sanitize(roomType);
 		return "INSERT INTO Rooms (roomName, roomDisplayName, roomDescription, roomID, labelX, labelY, roomType) VALUES('"+roomName
-				+"', '"+roomDisplayName+"', '"+roomDescription+"', "+roomID+","+ labelX +","+labelY+ ","+roomType+")";
+				+"','"+roomDisplayName+"','"+roomDescription+"',"+roomID+","+ labelX +","+labelY+ ",'"+roomType+"')";
 	}
 
 	public static String procInsertRoomWithLocation(int roomID, int nodeID, String roomName, String roomDisplayName, String roomDescription, double labelX, double labelY, String roomType){
 		roomName = sanitize(roomName);
 		roomDescription = sanitize(roomDescription);
 		return "INSERT INTO Rooms (roomName, roomDisplayName, roomDescription, nodeID, roomID, labelX, labelY, roomType) VALUES('"+roomName
-				+"', '"+roomDisplayName+"', '"+roomDescription+"', "+nodeID+", "+roomID+","+ labelX +","+labelY+","+roomType+ ")";
+				+"','"+roomDisplayName+"','"+roomDescription+"',"+nodeID+","+roomID+","+ labelX +","+labelY+",'"+roomType+ "')";
 	}
 
 	public static String procInsertEdge(int node1, int node2){
