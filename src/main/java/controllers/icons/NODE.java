c31fe5ce8d9be363c3488961b4a1728c300d3a9b
Jacob I. Komissar <git.i@komissar.net>
2017-04-28 13:46:55 -0400

    Add node color schemes for restricted node and portals

--- src/main/java/controllers/icons/NODE.java
+++ src/main/java/controllers/icons/NODE.java
@@ -10,12 +10,19 @@ enum NODE
 		implements ShapeScheme
 {
 	/* fill, strokeWidth, stroke, scale */
-	DESELECT(null, 1.5, Color.BLACK, 1.0),
+	SELECTED(null, 2.0, Color.BEIGE, 1.5),
+
 	DEFAULT(Color.BLUE, 1.5, Color.BLACK, 1.0),
-	ELEVATOR(Color.FUCHSIA, null, null, null),
 	ROOM(Color.YELLOW, null, null, null),
-	SELECTED(null, 2.0, Color.WHITE, 1.5),
+	ELEVATOR(Color.FUCHSIA, null, null, null),
+	PORTAL(Color.DARKORANGE, null, null, null),
+
+	RESTRICTED(Color.LIGHTBLUE, 1.5, Color.BLACK, 1.0),
+	RESTRICTED_ELEVATOR(Color.LIGHTPINK, null, null, null),
+	RESTRICTED_ROOM(Color.LIGHTYELLOW, null, null, null),
+	RESTRICTED_PORTAL(Color.ORANGE, null, null, null),
 	; // end of schemes
+
 	private final Color fill;
 	private final Double strokeWidth;
 	private final Color stroke;
