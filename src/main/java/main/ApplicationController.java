3a6083c77491ba48b4016232d65292af430ca11c
Robert Harrison <raharrison2@wpi.edu>
2017-04-29 16:49:06 -0400

    Added Welcome Screen to Application. when clicked on, main app runs.

--- src/main/java/main/ApplicationController.java
+++ src/main/java/main/ApplicationController.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import main.database.DatabaseWrapper;
 import main.database.DatabaseException;
@@ -53,7 +54,7 @@ public class ApplicationController extends Application
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		ApplicationController.stage = primaryStage;
-		Parent root = (BorderPane) FXMLLoader.load(this.getClass().getResource("/UserDestination.fxml"));
+		Parent root = (GridPane) FXMLLoader.load(this.getClass().getResource("/Welcome.fxml"));
 		primaryStage.setTitle("Faulkner Hospital Navigator");
 		Scene user = new Scene(root, 1174, 722);
 		primaryStage.setScene(user);
