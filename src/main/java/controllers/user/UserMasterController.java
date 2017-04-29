04756c6d952ac2543bae637cb9fe10510f0b8aa8
TheodoreJBieber <tbaslp@yahoo.com>
2017-04-29 16:59:36 -0400

    Made the about page not crash. Added an icon (resources/bwhIcon.png) to the window's header similar to the title. added a title and icon to the about page

--- src/main/java/controllers/user/UserMasterController.java
+++ src/main/java/controllers/user/UserMasterController.java
@@ -364,6 +364,8 @@ public class UserMasterController
 		loader.setLocation(this.getClass().getResource("/aboutPage.fxml"));
 		Scene addAboutScene = new Scene(loader.load());
 		Stage addAboutStage = new Stage();
+		addAboutStage.setTitle("Faulkner Hospital Navigator About Page");
+		addAboutStage.getIcons().add(new Image("bwhIcon.png"));
 		addAboutStage.initOwner(contentAnchor.getScene().getWindow());
 		addAboutStage.setScene(addAboutScene);
 		addAboutStage.showAndWait();
