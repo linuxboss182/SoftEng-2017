fa765c4025f875755423034741327ea0f3ecc12c
Jesse Gaulin <jtgaulin@autoreg-105599.dyn.wpi.edu>
2017-04-29 22:43:46 -0400

    Fixed map scaling to take into account the drawer

--- src/main/java/controllers/user/DrawerController.java
+++ src/main/java/controllers/user/DrawerController.java
@@ -86,4 +86,22 @@ abstract public class DrawerController
 		});
 	}
 
+	@Override
+	public void fitMapSize() {
+		double potentialY =
+				+ mapScroll.getHeight()/2
+				- contentAnchor.getHeight()/2;
+
+		double potentialX;
+		if(mainDrawer.isShown()) {
+			potentialX = (mapScroll.getWidth()+420) / 2
+					- contentAnchor.getWidth() / 2;
+		}else{
+			potentialX = (mapScroll.getWidth()) / 2
+					- contentAnchor.getWidth() / 2;
+		}
+
+		contentAnchor.setTranslateX(potentialX);
+		contentAnchor.setTranslateY(potentialY);
+	}
 }
